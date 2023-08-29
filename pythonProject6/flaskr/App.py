2ea844e36ef13c23dfaedb1780e508a218af517a
JUANES <juanes.kng.47@gmail.com>
2023-08-29 15:15:39 -0500

    3er update a db_clases correccion

--- pythonProject6/flaskr/App.py
+++ pythonProject6/flaskr/App.py
@@ -1,5 +1,5 @@
 from flaskr import create_app
-from .modelos import db, Usuario, Album, Medio
+from .modelos import db, Cancion, Usuario, Album, Medio
 
 app = create_app('default')
 app_context = app.app_context()
@@ -9,9 +9,12 @@ db.init_app(app)
 db.create_all()
 
 with app.app_context():
+    c = Cancion(titulo='prueba', minutos=2, segundos=25, interprete='artista')
     u = Usuario(nombre='juan', contrasena='12345')
-    a = Album(titulo='prueba', anio=1999, descripcion='texto',medio=Medio.CD)
+    a = Album(titulo='prueba', anio=1999, descripcion='text here', medio=Medio.CD)
+    a.canciones.append(c)
     u.albunes.append(a)
+    db.session.add(a)
     db.session.add(u)
     db.session.commit()
     print(Usuario.query.all())
@@ -19,3 +22,4 @@ with app.app_context():
     db.session.delete(u)
     print(Usuario.query.all())
     print(Album.query.all())
+    print(Cancion.query.all())
--- pythonProject6/flaskr/modelos/modelos.py
+++ pythonProject6/flaskr/modelos/modelos.py
@@ -3,26 +3,32 @@ import enum
 
 db = SQLAlchemy()
 
-class Cancion(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    titulo = db.Column(db.String(128))
-    minutos = db.Column(db.Integer)
-    segundos = db.Column(db.Integer)
-    interprete = db.Column(db.String(128))
-
 class Medio(enum.Enum):
     DISCO = 1
     CASETE = 2
     CD = 3
 
 class Album(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key = True)
     titulo = db.Column(db.String(128))
     anio = db.Column(db.Integer)
     descripcion = db.Column(db.String(512))
     medio = db.Column(db.Enum(Medio))
     usuario = db.Column(db.Integer, db.ForeignKey("usuario.id"))
+    canciones = db.relationship('Cancion', secondary='album_cancion', back_populates='album')
     __table_args__=(db.UniqueConstraint('usuario','titulo',name='titulo_unico_album'),)
+
+class Cancion(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    titulo = db.Column(db.String(128))
+    minutos = db.Column(db.Integer)
+    segundos = db.Column(db.Integer)
+    interprete = db.Column(db.String(128))
+    album = db.relationship('Album', secondary='album_cancion', back_populates='canciones')
+
+albunes_canciones = db.Table('album_cancion',\
+    db.Column('album_id', db.Integer, db.ForeignKey('album.id'), primary_key = True),\
+    db.Column('cancion_id', db.Integer, db.ForeignKey('cancion.id'), primary_key = True))
 
 class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
